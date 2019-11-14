3df55a7a02235627bcd9ef05b52e0ade90696e32
Marcin <rhoninn11@gmail.com>
2019-11-14 15:28:17 +0100

    simple api added

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ PROG = remote_farm
 
 SOURCES = $(PROG).c mongoose/mongoose.c cJSON/cJSON.c
 CFLAGS = -g -W -Wall -I./ -Wno-unused-function
+LIBS = -lm -pthread
 CC = gcc
 
 
 $(PROG): $(SOURCES)
-	$(CC) $(SOURCES) -o $@ $(CFLAGS)
+	$(CC) $(SOURCES) -o $@ $(CFLAGS) $(LIBS)
 
 clean:
 	rm -rf *.gc* *.dSYM *.exe *.obj *.o a.out $(PROG)
--- remote_farm.c
+++ remote_farm.c
@@ -3,43 +3,43 @@
 
 #include "mongoose/mongoose.h"
 #include "cJSON/cJSON.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <math.h>
 
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
-static int keep_listening = 1;
+static int keep_listening = 2;
 
-static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix) {
-  return uri->len >= prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
-}
+static double sin_value;
+static pthread_mutex_t sin_mutex;
 
-static void ev_handler(struct mg_connection *nc, int ev, void *p)
-{
+// functions
+static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix);
+static const char *create_value_JSON(float value);
+static void ev_handler(struct mg_connection *conn, int ev, void *p);
+static void send_sin_value(struct mg_connection *conn);
+static void signal_handler(int signal_code);
 
-	struct http_message *httpm = (struct http_message *)p;
-	const struct mg_str api_prefix = MG_MK_STR("/api");
+//threds
+void *sin_generateor(void *arg);
 
-	if (ev == MG_EV_HTTP_REQUEST)
-	{
-		if(has_prefix(&httpm->uri, &api_prefix)){
-			printf("\n -- api call: \n");
-			printf(httpm->method.p);
-		}
-		mg_serve_http(nc, httpm, s_http_server_opts);
-	}
-}
-
-static void signal_handler(int signal_code)
-{
-	printf("	--exiting program\n");
-	keep_listening = 0;
-}
 
 int main(void)
 {
+	//ptrhread initialization
+	pthread_t sin_generator_thread;
+	
+	pthread_mutex_init(&sin_mutex, NULL);
+	pthread_create(&sin_generator_thread,NULL, sin_generateor, NULL);
+
+	//mongoose initialization
 	struct mg_mgr mgr;
 	struct mg_connection *nc;
 
 	signal(SIGINT, signal_handler);
+	signal(SIGKILL, signal_handler);
+	signal(SIGTERM, signal_handler);
 
 	mg_mgr_init(&mgr, NULL);
 	printf("Starting web server on port %s\n", s_http_port);
@@ -52,7 +52,7 @@ int main(void)
 
 	// Set up HTTP server parameters
 	mg_set_protocol_http_websocket(nc);
-	s_http_server_opts.document_root = "./remote_farm_client/build"; // Serve current directory
+	s_http_server_opts.document_root = "./client/build"; // Serve current directory
 	s_http_server_opts.enable_directory_listing = "yes";
 
 	while (keep_listening)
@@ -60,7 +60,86 @@ int main(void)
 		mg_mgr_poll(&mgr, 1000);
 	}
 
+	pthread_mutex_destroy(&sin_mutex);
 	mg_mgr_free(&mgr);
 
 	return 0;
+}
+
+static int has_prefix(const struct mg_str *uri, const struct mg_str *prefix)
+{
+	return uri->len >= prefix->len && memcmp(uri->p, prefix->p, prefix->len) == 0;
+}
+
+void *sin_generateor(void *arg)
+{
+	double time = 0;
+	while (keep_listening)
+	{
+		pthread_mutex_lock(&sin_mutex);
+		sin_value = sin(time);
+		pthread_mutex_unlock(&sin_mutex);
+
+		time += 0.01;
+		time = (time > 2 * M_PI) ? (time - 2 * M_PI) : time;
+		usleep(10 * 1000); //10 ns
+	}
+
+	return;
+}
+
+static const char *create_value_JSON(float value)
+{
+	char *jsonAsString;
+	cJSON *numberValue = NULL;
+
+	cJSON *object = cJSON_CreateObject();
+	if (object == NULL)
+		return NULL;
+
+	numberValue = cJSON_CreateNumber(value);
+	if (numberValue == NULL)
+		return NULL;
+	cJSON_AddItemToObject(object, "value", numberValue);
+
+	jsonAsString = cJSON_Print(object);
+	cJSON_Delete(object);
+
+	return (const char *)jsonAsString;
+}
+
+static void ev_handler(struct mg_connection *conn, int ev, void *p)
+{
+
+	struct http_message *httpm = (struct http_message *)p;
+	const struct mg_str api_prefix = MG_MK_STR("/api");
+
+	if (ev == MG_EV_HTTP_REQUEST)
+	{
+		if (has_prefix(&httpm->uri, &api_prefix))
+		{
+			send_sin_value(conn);
+		}
+		mg_serve_http(conn, httpm, s_http_server_opts);
+	}
+}
+
+static void send_sin_value(struct mg_connection *conn)
+{
+	printf("\n -- api call: \n");
+
+	pthread_mutex_lock(&sin_mutex);
+	double tsin_value = sin_value;
+	pthread_mutex_unlock(&sin_mutex);
+	char *v_json = create_value_JSON(tsin_value);
+
+	mg_printf(conn, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\nContent-Type: application/json\r\n\r\n");
+	mg_printf_http_chunk(conn, v_json);
+	mg_send_http_chunk(conn, "", 0); /* Send empty chunk, the end of response */
+}
+
+static void signal_handler(int signal_code)
+{
+	printf("	--exiting program\n");
+	keep_listening = 0;
 }
